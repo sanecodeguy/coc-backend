130d602bfdc774eb148b0e99c4797c3103c55869
Syed Ali Rizvi <rizvicottage2@gmail.com>
2026-04-28 21:00:46 +0500

    Added vercel url

--- main.py
+++ main.py
@@ -176,7 +176,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://cloutorcancel.live", "https://www.cloutorcancel.live"],
+    allow_origins=["https://cloutorcancel.live", "https://www.cloutorcancel.live","https://clout-or-cancel.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
